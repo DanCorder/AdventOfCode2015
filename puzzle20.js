c8daa2899373332fc33419dfa7748fbac4da2047
Dan Corder <dev@dancorder.com>
2015-12-10 07:31:22 +0000

    Puzzle 20

---
+++ puzzle20.js
@@ -0,0 +1,37 @@
+(function() {
+    var input = '1113122113';
+    
+    var stepCount = 0;
+    
+    while (stepCount < 50) {
+        input = nextStep(input);
+        stepCount++;
+    }
+    
+    console.log(input.length);
+})();
+
+function nextStep(input) {
+    var nextStep = '';
+    
+    var currentRunChar = '';
+    var runLength = 1;
+    
+    for (var i = 0; i < input.length; i++) {
+        var char = input.charAt(i);
+        
+        if (char !== currentRunChar) {
+            if (currentRunChar !== '') {
+                nextStep += runLength + currentRunChar;
+            }
+            currentRunChar = char;
+            runLength = 1;
+        } else {
+            runLength++;
+        }
+    }
+    
+    nextStep += runLength + currentRunChar;
+
+    return nextStep;
+}
